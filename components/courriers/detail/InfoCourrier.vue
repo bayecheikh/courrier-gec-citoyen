db297bd31eb403b579c3fdcb42c76fb9f619d3e9
Cheikh Gueye <bayecheikhgueye2@gmail.com>
2023-06-03 17:09:13 +0000

    Update

--- components/courriers/detail/InfoCourrier.vue
+++ components/courriers/detail/InfoCourrier.vue
@@ -8,15 +8,15 @@
       </v-row>  -->
       
       <v-row class="border-grey mb-5 pl-5 pr-5 pb-5">
-        <v-col md="12" sm="12" lg="12" text-md-left class="row d-flex justify-end ">
+        <v-col md="12" sm="12" lg="12" class="row d-flex justify-start ">
           <div class="col-md-6 col-sm-12 col-lg-6 ">
             <div class="d-flex text-label mb-5">Objet : <div class="text-green text-value">{{this.detailCourrier && this.detailCourrier.subject}}</div> </div>
             <div class="d-flex text-label mb-5">Organisme : <div class="text-green text-value">{{this.detailCourrier && this.detailCourrier.structure && this.detailCourrier.structure.description}}</div> </div>
             <div class="d-flex text-label mb-5">Code de suivi : <div class="text-green text-value">{{this.detailCourrier && this.detailCourrier.idSuivi}}</div> </div>
           </div>
           <div class="col-md-6 col-sm-12 col-lg-6 ">
-            <div class="d-flex text-label mb-5">Date d'envoi : <div class="text-green text-value">{{this.detailCourrier && this.detailCourrier.createdAt}}</div> </div>
-            <div class="d-flex text-label mb-5">Date de réception : <div class="text-green text-value">{{this.detailCourrier && this.detailCourrier.documentDate}}</div> </div>
+            <div class="d-flex text-label mb-5">Date d'envoi : <div class="text-green text-value">{{$getDateFormat(this.detailCourrier && this.detailCourrier.createdAt)}}</div> </div>
+            <div class="d-flex text-label mb-5">Date de réception : <div class="text-green text-value">{{$getDateFormat(this.detailCourrier && this.detailCourrier.documentDate)}}</div> </div>
             <div class="d-flex text-label mb-5">Statut : <div class="text-green text-value ml-3 mb-5"><v-chip 
                 :color="$getColore(this.detailCourrier && this.detailCourrier.traitement_status_slug)"
                 outlined
@@ -24,9 +24,11 @@
               {{ $getStatus(this.detailCourrier && this.detailCourrier.traitement_status_slug) }}
             </v-chip></div> </div>
           </div>
-          <div class="col-md-12 col-sm-12 col-lg-12 border-grey " v-if="this.detailCourrier.responses.length">
-            <div class="d-flex text-label mb-5">Objet: <div class="text-green text-value"> {{this.detailCourrier && this.detailCourrier.responses && this.detailCourrier.responses[0] && this.detailCourrier.responses[0].object}}</div> </div>
-          <div class="d-flex text-label mb-5">Reponse : <div class="text-green text-value">{{this.detailCourrier && this.detailCourrier.responses && this.detailCourrier.responses[0] && this.detailCourrier.responses[0].body}}</div> </div>
+          
+          <div class="col-md-12 col-sm-12 col-lg-12 border-grey " v-if="this.detailCourrier.responses && this.detailCourrier.responses.length">
+            <h3 class="mb-5">Réponse</h3>
+            <div class="d-flex text-label mb-5">Objet: <span v-html="this.detailCourrier && this.detailCourrier.responses && this.detailCourrier.responses[0] && this.detailCourrier.responses[0].object"></span></div>
+          <div class="d-flex text-label mb-5"><div class="text-green text-value" v-html="this.detailCourrier && this.detailCourrier.responses && this.detailCourrier.responses[0] && this.detailCourrier.responses[0].body"></div> </div>
           </div>
           
         </v-col>
@@ -36,7 +38,7 @@
           <div class="col-md-12 col-sm-12 col-lg-12">
             <div class="d-flex text-label mb-5"><h2>Pièces-jointes</h2></div>
             
-            <div class="card" v-if="this.detailCourrier.pieces_jointes.length">
+            <div class="card" v-if="this.detailCourrier.pieces_jointes && this.detailCourrier.pieces_jointes.length">
                 <ul class="list-group list-group-flush d-flex">
                       <li 
                         v-for="(file, index) in this.detailCourrier.pieces_jointes"
@@ -53,7 +55,7 @@
           </div>
         </v-col>
       </v-row>
-      <v-row class="border-grey mb-5">
+      <v-row class="border-grey mb-5" v-if="this.detailCourrier.responses && !this.detailCourrier.responses.length">
         <v-col md="12" sm="12" lg="12" text-md-left class="row d-flex justify-end ">
           <div class="col-md-12 col-sm-12 col-lg-12">
             <div class="d-flex text-label mb-5"><h2>Courrier principal</h2></div>
